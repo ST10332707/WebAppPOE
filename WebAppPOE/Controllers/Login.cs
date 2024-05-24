046497b2e63fedf99a79ffaf6e1579fb256ac8b9
Unathi <Unathikolberg17@gmail.com>
2024-05-24 18:13:19 +0200

    Changes

--- WebAppPOE/Controllers/Login.cs
+++ WebAppPOE/Controllers/Login.cs
@@ -13,22 +13,23 @@ namespace WebAppPOE.Controllers
             login = new Login_Model();
         }
 
+        //post or get user id in home page when user has logged in successfully and check if the user account exists
         [HttpPost]
         public ActionResult Privacy(string email, string name)
         {
             var loginModel = new Login_Model();
             int userID = loginModel.SelectUser(email, name);
-            if (userID != -1)
+            if (userID != -1)//if statement, to check if user account exists
             {
                 // Store userID in session
                 HttpContext.Session.SetInt32("UserID", userID);
 
-                // User found, proceed with login logic
+                // User account is valid, proceed with login logic
                 return RedirectToAction("Index", "Home");
             }
             else
             {
-                // User not found
+                // User account does not exist, error
                 return View("LoginFailed");
             }
         }
---
+++ WebAppPOE/Controllers/ProductDisplay.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAppPOE.Models;
+
+namespace WebAppPOE.Controllers
+{
+    public class ProductDisplay : Controller
+    {
+        //Get data from the selectProducts and post them in the view
+        [HttpGet]
+        public IActionResult MyWork(int id, string name, decimal price, string category, bool availability)
+        {
+            var products = Product_Display.SelectProducts();
+            return View(products);
+        }
+    }
+}
